76b06663fdfd37c2c5cf8ea627ff5f60c1b7b06f
MilkMGN <42798255+MilkMGN@users.noreply.github.com>
2023-03-10 10:59:11 +1000

    Update merge.py

--- src/merge.py
+++ src/merge.py
@@ -138,12 +138,14 @@ def splash():
         if serial_bus == "":
             print("\u001b[35;1mYou need to set your Serial bus in settings.\n | You can find it by running \ndmesg | grep tty. Set the setting to whatever it prints, e.g: ttyUSB0\u001b[0m")
         else:
-            port == dmx.select_port("/dev/" + serial_bus, auto=False)
+            # port == dmx.select_port("/dev/" + serial_bus, auto=False)
+            port == dmx.DMXConnection('/dev/serial/by-id/' + serial_bus)
     elif grabOS == "Windows":
         if serial_bus == "":
             print("\u001b[35;1mYou need to set your Serial bus in settings.\n | You can find it by going to Device manager, and looking for 'Ports (COM & LPT)'. Set the setting to whatever it shows in brackets, \u001b[33;1me.g: COM3\u001b[0m")
         else:
-            port == dmx.select_port(serial_bus, auto=False)
+            # port == dmx.select_port(serial_bus, auto=False)
+            port == dmx.DMXConnection(serial_bus)
     elif serial_bus == "NONE": 
     #print(port.lower())
         pass
