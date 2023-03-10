d9623a3a97783d0a41369bc150c0d7e124a3b00e
MilkMGN <42798255+MilkMGN@users.noreply.github.com>
2023-03-10 10:35:26 +1000

    Update login.py

--- src/login.py
+++ src/login.py
@@ -10,30 +10,30 @@ class globalvar:
         return Ver
 
 globalvar.version()
-os.system('cls')
+os.system('clear')
 print("\u001b[31mMergeartnet \u001b[32mV",Ver,"\u001b[0m\n\u001b[31mProduction Enviroment, BE CAREFULL\u001b[0m")
 global username
 database = {"Ben": "Oasis2023$", "admin": "root"}
 username = input("\u001b[32mEnter Your Username : \u001b[0m")
-os.system('cls')
+os.system('clear')
 print("\u001b[35mWelcome! <\u001b[32m",username,"\u001b[0m\u001b[35m>. Please enter your password.\n\u001b[0m")
 password = getpass("\u001b[32mEnter Your Password : \u001b[0m")
 for i in database.keys():
     if username == i:
         while password != database.get(i):
             while f != 3:
-                os.system('cls')
+                os.system('clear')
                 f = f + 1
                 print("\u001b[31mIncorrect Attempts ",f,"/3\u001b[0m")
                 password = getpass("\u001b[32mEnter Your Password Again : \u001b")
                 if f == 3:
-                    os.system('cls')
+                    os.system('clear')
                     print("\u001b[31mLogin Failed, Too many attempts, Contact Administrator\u001b[0m")
                     time.sleep(5)
                     quit()
                 break
 
-        os.system('cls')
+        os.system('clear')
         print("\u001b[33mWelcome To SillyMergeArtNet!! Try not to break it...\u001b[0m")
         os.system("python merge.py")
        
